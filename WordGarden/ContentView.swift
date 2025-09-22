df3ad645dd8d5053c32d8d589bde4e3b0835dd7b
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-09-21 23:21:27 -0400

    Initial Commit

---
+++ WordGarden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordGarden
+//
+//  Created by Jeff Gutierrez on 9/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
